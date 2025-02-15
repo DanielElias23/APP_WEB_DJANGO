b5f883eadc4446cbfbba3c7527f941eb664341f5
daniel <danielchingasilva@gmail.com>
2025-02-15 09:53:10 -0300

    nada

--- api_datos/settings.py
+++ api_datos/settings.py
@@ -82,7 +82,10 @@ WSGI_APPLICATION = 'api_datos.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+    default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+    conn_max_age=600
+)
 }
 
 
